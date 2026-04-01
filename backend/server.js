9f0d3ff87e3e049335cd6d7a00abe912bdb4ee15
Nathan <admin@pickthegame.com>
2026-04-01 19:02:28 -0400

    Added completed api endpoint

--- backend/server.js
+++ backend/server.js
@@ -27,12 +27,11 @@ export const handler = async (event) => {
                 },
                 body: ''
             };
-        } else if (method === 'GET' && path.startsWith('/todos')) {
-          const postId = path.split('/').length > 2 ? path.split('/')[2] : null;
-          if(postId){
-            return await updateTodo(postId, { completed: true });
-          } 
-          return await getTodos();
+        } else if (method === 'GET' && path === '/todos') {
+            return await getTodos();
+        }else if(method === 'GET' && path.startsWith('/completed/')) {
+            const id = path.split('/')[2];
+            return await updateTodo(id, { completed: true });
         } else if (method === 'POST' && path === '/todos') {
             const body = parseRequestBody(event);
             return await createTodo(body);
